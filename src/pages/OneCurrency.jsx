699a5625671ffb442d6413c7124020dcad3095de
Yokub1 <yokubibnsafar@gmail.com>
2022-03-18 01:08:38 +0500

    improvements

--- src/pages/OneCurrency.jsx
+++ src/pages/OneCurrency.jsx
@@ -10,30 +10,42 @@ const OneCurrency = () => {
     // configuring date
     let month = String(new Date().getMonth());
     let result = [];
+    let days = [];
     if (month.length === 1) {
         month = "0" + month;
     }
+    // getting data for 10 days
     for (let i = 0; i < 10; i++) {
         let datet = String(new Date().getDate() - i)
         if (datet.length < 2) {
             datet = "0" + datet;
         }
         let today = new Date().getFullYear() + "/" + month + "/" + datet
+        days.push(today)
         axios.get(API_PATH + today + "/daily_json.js").then((res) => {
-            // console.log(res.data)
-            console.log(res.data.Valute)
+            // console.log(res.data.Valute)
+            Object.entries(res.data.Valute).map(item => {
+                if (item[1].NumCode == code) {
+                    result.push(item)
+                    // console.log(result)
+                }
+            })
         }).catch((err) => {
             console.log(err)
         })
     }
     useEffect(() => {
-
     }, [])
 
     return (
         <>
             <div className="currency-wrapper">
                 <h1>currency code: {params.code}</h1>
+                {result.map((item, index) => {
+                    return <p key={index}>
+                        {item[0]}
+                    </p>
+                })}
             </div>
         </>
     )
